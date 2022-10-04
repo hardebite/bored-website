6d89b0dfa694d59997b019b8f79cb360db26a7f7
hardebite <adedoyinadegbite.s@gmail.com>
2022-10-04 18:00:21 +0100

    add javascript and animation

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
 from flask import Flask, render_template, redirect,url_for,request, flash,send_from_directory
 from flask_bootstrap import Bootstrap
 import requests
-URL="http://www.boredapi.com/api/activity"
 app = Flask(__name__)
 Bootstrap(app)
 form=[]
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 @app.route('/')
 def home():
-    info = requests.get(URL)
-    form = info.json()
-    print(form)
-    form =form["activity"]
-
-    return render_template("index.html", form=form)
-
-
+    return render_template("index.html")
 
 if __name__=="__main__":
     app.run(debug=True)
