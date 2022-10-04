3aca77cd5a6686ad17f2787534382d43e0f38eff
hardebite <adedoyinadegbite.s@gmail.com>
2022-10-04 18:10:56 +0100

    add javascript

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, redirect,url_for,request, flash,send_from_directory
 from flask_bootstrap import Bootstrap
-import requests
+
 app = Flask(__name__)
 Bootstrap(app)
 form=[]
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", form=form)
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
